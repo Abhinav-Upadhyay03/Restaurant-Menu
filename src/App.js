83684aeba9fe5626312f6bd999556f62bd231e08
Abhinav Upadhyay <abhinavupadhyay822@gmail.com>
2024-05-25 17:07:43 +0530

    all files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Restraunt from './components/Basics/Restraunt'
+
+const App = () => {
+  return (
+    <>
+      <Restraunt />
+    </>
+  )
+}
+
+export default App
